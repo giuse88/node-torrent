307327dbe0370425a0a670696f630c49c7a2e462
Giuseppe <giuse88@gmail.com>
2013-12-22 00:27:03 +0100

    moved to bencode library

---
+++ client.js
@@ -0,0 +1,10 @@
+var Client = require('./lib/client.js');
+var client = new Client({logLevel: 'DEBUG'});
+var torrent = client.addTorrent('sample.torrent');
+
+// when the torrent completes, move it's files to another area
+torrent.on('complete', function() {
+    console.log('complete!');
+    torrent.files.forEach(function(file) {
+    });
+});
